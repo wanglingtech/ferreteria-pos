be61553bff98ec4ed0a31907b9695aeb0dfb3195
Wangling <kevinvillegassolisxd@gmail.com>
2026-05-31 15:39:13 -0500

    Frontend de Productos + Inventario Visualmente

--- frontend/ferreteriaApp/src/app/inventario/pages/inventario.page.ts
+++ frontend/ferreteriaApp/src/app/inventario/pages/inventario.page.ts
@@ -1,12 +1,57 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { IonCard, IonCardContent, IonContent, IonText } from '@ionic/angular/standalone';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import {
+  IonCard,
+  IonCardContent,
+  IonContent,
+  IonSearchbar,
+  IonList,
+  IonItem,
+  IonLabel,
+  IonBadge,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-inventario-page',
   standalone: true,
   templateUrl: './inventario.page.html',
   styleUrls: ['./inventario.page.scss'],
-  imports: [CommonModule, IonContent, IonCard, IonCardContent, IonText],
+  imports: [
+    CommonModule,
+    IonContent,
+    IonCard,
+    IonCardContent,
+    IonSearchbar,
+    IonList,
+    IonItem,
+    IonLabel,
+    IonBadge,
+
+    // 🔴 ESTO FALTABA
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+  ],
 })
-export class InventarioPage {}
+export class InventarioPage implements OnInit {
+  resumen: any = null;
+  lowStockProducts: any[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.loadResumen();
+  }
+
+  loadResumen() {
+    this.http.get('http://localhost:3000/inventario/resumen').subscribe({
+      next: (data) => (this.resumen = data),
+      error: (err) => console.error(err),
+    });
+  }
+}
